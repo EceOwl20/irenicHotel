00b52efb16709e170af0ea9269c72d0692fee24b
Ece Bayramer <ecebayramer20@gmail.com>
2025-05-07 16:47:22 +0300

    videos added

--- client/app/[locale]/generalComponents/MainBanner.jsx
+++ client/app/[locale]/generalComponents/MainBanner.jsx
@@ -17,7 +17,7 @@ const MainBanner = () => {
         playsInline
         className=" top-0 left-0 w-full h-full object-cover hidden lg:flex relative">
         <source 
-          src="/videos/irenic.mp4"
+          src="/videos/irenicDesktop.mp4"
           type="video/mp4"
         />
         Tarayıcınız bu videoyu desteklemiyor.
@@ -32,12 +32,13 @@ const MainBanner = () => {
         className="absolute top-0 left-0 w-full h-full object-cover flex lg:hidden"
       >
         <source 
-          src="/videos/irenic.mp4"
+          src="/videos/irenicMobile.mp4"
           type="video/mp4"
         />
         Tarayıcınız bu videoyu desteklemiyor.
         
       </video>
+      
       {/* <div className='flex flex-col justify-center items-center text-center text-white z-20 gap-[20px]'>
         <span className='text-[14px] md:text-[16px] lg:text-[20px] font-medium font-heebo'>{t("subtitle")}</span>
         <h2 className='text-[32px] md:text-[40px] lg:text-[52px] font-montserrat font-medium text-white filter drop-shadow-lg'>{t("title")}</h2>
