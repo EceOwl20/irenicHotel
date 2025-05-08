f5c1e89d7e26d9599ae8cf1473717e0687ff2604
Ece Bayramer <ecebayramer20@gmail.com>
2025-05-08 15:12:43 +0300

    intl edited

--- client/i18n/routing.js
+++ client/i18n/routing.js
@@ -9,12 +9,80 @@ export const routing = defineRouting({
   localeDetection: true,
   localePrefix : "always",
   pathnames : {
-    "/": {
-      tr: "/",
-      en: "/",
-      de: "/",
-    ru: "/",
-    ae:"/"
+    '/': {
+      tr: '/',   // /tr/
+      en: '/',   // /en/
+      de: '/',   // /de/
+      ru: '/',   // /ru/
+      ae: '/'    // /ae/
+    },
+
+    '/aboutus': {
+      tr: '/hakkimizda',  // /tr/hakkimizda
+      en: '/about',       // /en/about
+      de: '/uber-uns',    // /de/uber-uns
+      ru: '/o-nas',       // /ru/o-nas
+      ae: '/about'        // /ae/about
+    },
+
+    '/contact': {
+      tr: '/iletisim',
+      en: '/contact',
+      de: '/kontakt',
+      ru: '/kontakt',
+      ae: '/contact',
+    },
+
+    '/rooms': {
+      tr: '/odalar',
+      en: '/rooms',
+      de: '/zimmer',
+      ru: '/nomera',
+      ae: '/rooms',
+    },
+
+   '/rooms/deluxe-room-fr': {
+      tr: '/odalar/deluxe-room-fr',
+      en: '/rooms/deluxe-room-fr',
+      de: '/zimmer/deluxe-room-fr',
+      ru: '/nomera/deluxe-room-fr',
+      ae: '/rooms/deluxe-room-fr'
+    },
+    '/rooms/deluxe-room-tw': {
+      tr: '/odalar/deluxe-room-tw',
+      en: '/rooms/deluxe-room-tw',
+      de: '/zimmer/deluxe-room-tw',
+      ru: '/nomera/deluxe-room-tw',
+      ae: '/rooms/deluxe-room-tw'
+    },
+    '/rooms/corner-room': {
+      tr: '/odalar/corner-room',
+      en: '/rooms/corner-room',
+      de: '/zimmer/corner-room',
+      ru: '/nomera/corner-room',
+      ae: '/rooms/corner-room'
+    },
+    '/rooms/jacuzzi-suite': {
+      tr: '/odalar/jacuzzi-suite',
+      en: '/rooms/jacuzzi-suite',
+      de: '/zimmer/jacuzzi-suite',
+      ru: '/nomera/jacuzzi-suite',
+      ae: '/rooms/jacuzzi-suite'
+    },
+    '/rooms/deluxe-single-room': {
+      tr: '/odalar/deluxe-single-room',
+      en: '/rooms/deluxe-single-room',
+      de: '/zimmer/deluxe-single-room',
+      ru: '/nomera/deluxe-single-room',
+      ae: '/rooms/deluxe-single-room'
+    },
+  
+    '/services': {
+      tr: '/hizmetler',
+      en: '/services',
+      de: '/leistungen',
+      ru: '/uslugi',
+      ae: '/services',
     },
 
   }})
