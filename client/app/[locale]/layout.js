01db6e1c9b9fd746fa51c17db1a7fb493de3dff8
Ece Bayramer <ecebayramer20@gmail.com>
2025-06-02 11:26:57 +0300

    tagg added

--- client/app/[locale]/layout.js
+++ client/app/[locale]/layout.js
@@ -41,6 +41,7 @@ export function generateStaticParams() {
 
 export default async function RootLayout({ children, params }) {
   const { locale } = await params;
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
 
   if (!routing.locales.includes(locale)) {
     notFound();
@@ -73,7 +74,20 @@ export default async function RootLayout({ children, params }) {
           `}
         </Script>
 
-
+          <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'? '&l='+l : '';
+              j.async = true;
+              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
 
 
           {/* Chat script'i sabit olarak layout'ta bırakıyoruz */}
@@ -110,6 +124,16 @@ export default async function RootLayout({ children, params }) {
       <body
         className={`${montserrat.variable} ${heebo.variable} antialiased`}
       >
+
+             <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
           <NextIntlClientProvider locale={locale} messages={messages}>
             <Header/>
             <BookSection/>
